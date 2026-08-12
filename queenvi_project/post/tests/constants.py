64d4ccf3846d9336241ef9771922b6ec6ab2575e
Tim <bikma2004@gmail.com>
2026-08-12 23:07:33 +0300

    full post list

--- queenvi_project/post/tests/constants.py
+++ queenvi_project/post/tests/constants.py
@@ -1,3 +1,28 @@
+from user.constants import UserRole
+
+
+class ListPostConstants:
+    FIRST_POST_IDX = 0
+    NO_COMMENTS = 0
+    NO_LIKES = 0
+    NO_POSTS = 0
+    ONE_COMMENT = 1
+    ONE_LIKE = 1
+    ONE_POST = 1
+    FILTER_PARAMS = [
+        (None, True,),
+        (None, False,),
+        (UserRole.USER, True,),
+        (UserRole.USER, False,),
+        (UserRole.MODER, True,),
+        (UserRole.MODER, False,),
+        (UserRole.STREAMER, True,),
+        (UserRole.STREAMER, False,),
+    ]
+    SECOND_POST_IDX = 1
+    TWO_POSTS = 2
+
+
 class ImageConstants:
     CONTENT_TYPE = 'image/jpeg'
     FIRST_MEDIA_IDX = 0
